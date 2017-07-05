1bb57fbd4630ab95555e544bf56838332249ea20
Tapio Sivonen <tapio.sivonen@kotikone.fi>
2017-07-05 23:43:57 +0300

    cleanup for charts

--- src/interest/BOFEuriborFetcher.java
+++ src/interest/BOFEuriborFetcher.java
@@ -15,181 +15,188 @@ public class BOFEuriborFetcher
 
     public static final String BOF_EURIBOR_URI
     //="http://www.suomenpankki.fi/en/Statistics/interest-rates/"
-	= "http://www.suomenpankki.fi/fi/Tilastot/korot/";
+        = "http://www.suomenpankki.fi/fi/Tilastot/korot/";
     public static final String EURIBOR_TABLE_CLASS
-	= "tstyle1 zebra";
+        = "tstyle1 zebra";
 
     private int parseDuration(String durationText)
     {
-	String[] parts
-	    = durationText
-	    .trim()
-	    .split(" ");
-	int count
-	    = Integer.parseInt(parts[0]);
-	int multiplier
-	    = "kk".equals(parts[1])
-	    ? 30 : 7; //kk -> month, 30 days; others -> week, 7 days
-	return count*multiplier;
+        String[] parts
+            = durationText
+            .trim()
+            .split(" ");
+        int count
+            = Integer.parseInt(parts[0]);
+        int multiplier
+            = "kk".equals(parts[1])
+            ? 30 : 7; //kk -> month, 30 days; others -> week, 7 days
+        return count*multiplier;
     }
 
     private Date parseDate(String dateText)
     {
-	SimpleDateFormat fiDate
-	    = new SimpleDateFormat("d.M.yyyy");
-	try {
-	    return fiDate.parse(dateText.trim());
-	}
-	catch(ParseException pe) {
-	    return null;
-	}
+        SimpleDateFormat fiDate
+            = new SimpleDateFormat("d.M.yyyy");
+        try {
+            return fiDate.parse(dateText.trim());
+        }
+        catch(ParseException pe) {
+            return null;
+        }
     }
 
     private double parseRate(String rateText)
     {
-	NumberFormat fiDouble
-	    = NumberFormat
-	    .getInstance(new Locale("fi","FI"));
-	try {
-	    return 0.01d*fiDouble //the rates are in percentages
-		.parse(rateText.trim())
-		.doubleValue();
-	}
-	catch(ParseException pe) {
-	    return Double.NaN;
-	}
+        NumberFormat fiDouble
+            = NumberFormat
+            .getInstance(new Locale("fi","FI"));
+        try {
+            return 0.01d*fiDouble //the rates are in percentages
+                .parse(rateText.trim())
+                .doubleValue();
+        }
+        catch(ParseException pe) {
+            return Double.NaN;
+        }
     }
 
     private Stream<InterestRateEntry>
-	streamInterestRateRow(Element row,
-			      final int[] durationDays)
+        streamInterestRateRow(Element row,
+                              final int[] durationDays)
     {
-	Elements rowChildren
-	    = row.children();
-	final Date rowDate
-	    = parseDate(rowChildren.get(0).text());
-	if(null == rowDate)
-	    return Stream.empty();
-	return
-	    IntStream
-	    .range(0, durationDays.length)
-	    .parallel()
-	    .mapToObj(columnIndex ->
-		      new InterestRateEntry
-		      ("EURIBOR",
-		       rowDate,
-		       parseRate(rowChildren
-				 .get(columnIndex+1)
-				 .text()
-				 ),
-		       durationDays[columnIndex]
-		       )
-		      )
-	    .filter(rateEntry ->
-		    null != rateEntry.getDate()
-		    );
+        Elements rowChildren
+            = row.children();
+        final Date rowDate
+            = parseDate(rowChildren.get(0).text());
+        if(null == rowDate)
+            return Stream.empty();
+        return
+            IntStream
+            .range(0, durationDays.length)
+            .parallel()
+            .mapToObj(columnIndex ->
+                      new InterestRateEntry
+                      ("EURIBOR",
+                       rowDate,
+                       parseRate(rowChildren
+                                 .get(columnIndex+1)
+                                 .text()
+                                 ),
+                       durationDays[columnIndex]
+                       )
+                      )
+            .filter(rateEntry ->
+                    null != rateEntry.getDate()
+                    );
     }
 
     public BOFEuriborFetcher()
-	throws IOException
+        throws IOException
     {
-	BOFEuriborDocument
-	    = Jsoup.connect(BOF_EURIBOR_URI).get();
+        BOFEuriborDocument
+            = Jsoup.connect(BOF_EURIBOR_URI).get();
     }
     
     public Stream<InterestRateEntry> interestRateEntryStream()
     {
-	Element euriborTable
-	    = BOFEuriborDocument
-	    //.body()
-	    .getElementsByClass(EURIBOR_TABLE_CLASS)
-	    .get(0); //first table with class EURIBOR_TABLE_CLASS
-	Elements euriborTableRows
-	    = euriborTable
-	    .getElementsByTag("tr");
+        Element euriborTable
+            = BOFEuriborDocument
+            //.body()
+            .getElementsByClass(EURIBOR_TABLE_CLASS)
+            .get(0); //first table with class EURIBOR_TABLE_CLASS
+        Elements euriborTableRows
+            = euriborTable
+            .getElementsByTag("tr");
 
-	Elements durationHeaders
-	    = euriborTableRows
-	    .get(1)
-	    .getElementsByTag("th");
+        Elements durationHeaders
+            = euriborTableRows
+            .get(1)
+            .getElementsByTag("th");
 
-	int dataColumns
-	    = durationHeaders.size()-1;
-	int[] durationDays
-	    = new int[dataColumns];
-	Arrays
-	    .parallelSetAll(durationDays,
-			    dayIndex ->
-			    parseDuration(durationHeaders
-					  .get(dayIndex+1)
-					  .text()
-					  )
-			    );
-	return
-	    euriborTableRows
-	    .subList(2, euriborTableRows.size())
-	    .parallelStream()
-	    .flatMap(row ->
-		     streamInterestRateRow(row, durationDays)
-		     );
+        int dataColumns
+            = durationHeaders.size()-1;
+        int[] durationDays
+            = new int[dataColumns];
+        Arrays
+            .parallelSetAll(durationDays,
+                            dayIndex ->
+                            parseDuration(durationHeaders
+                                          .get(dayIndex+1)
+                                          .text()
+                                          )
+                            );
+        return
+            euriborTableRows
+            .subList(2, euriborTableRows.size())
+            .parallelStream()
+            .flatMap(row ->
+                     streamInterestRateRow(row, durationDays)
+                     );
     }
 
     private NavigableSet<InterestRateEntry>
-	durationComparingInterestRateEntrySet(InterestRateEntry... initialEntries)
+        durationComparingInterestRateEntrySet
+        (InterestRateEntry... initialEntries)
     {
-	return durationComparingInterestRateEntrySet(Arrays.asList(initialEntries));
+        return durationComparingInterestRateEntrySet
+            (Arrays.asList(initialEntries));
     }
     private NavigableSet<InterestRateEntry>
-	durationComparingInterestRateEntrySet(Collection<InterestRateEntry> initialEntries)
+        durationComparingInterestRateEntrySet
+        (Collection<InterestRateEntry> initialEntries)
     {
-	NavigableSet<InterestRateEntry> result
-	    = new ConcurrentSkipListSet<InterestRateEntry>
-	    ((left, right)
-	     -> left.getDurationDays() - right.getDurationDays()
-	     );
-	result.addAll(initialEntries);
-	return result;
+        NavigableSet<InterestRateEntry> result
+            = new ConcurrentSkipListSet<InterestRateEntry>
+            ((left, right)
+             -> left.getDurationDays() - right.getDurationDays()
+             );
+        result.addAll(initialEntries);
+        return result;
     }
 
-    private java.util.stream.Collector<InterestRateEntry,?,NavigableSet<InterestRateEntry>> toSetByDuration()
+    private java.util.stream.Collector
+        <InterestRateEntry,?,NavigableSet<InterestRateEntry>>
+        toSetByDuration()
     {
-	return
-	    Collectors.
-	    collectingAndThen(Collectors.toList(),
-			      list -> durationComparingInterestRateEntrySet(list)
-			      );
+        return
+            Collectors.
+            collectingAndThen(Collectors.toList(),
+                              list ->
+                              durationComparingInterestRateEntrySet(list)
+                              );
     }
     
-    public NavigableMap<Date, NavigableSet<InterestRateEntry>> interestRateEntryMap()
+    public NavigableMap<Date, NavigableSet<InterestRateEntry>>
+        interestRateEntryMap()
     {
-	return
-	    new TreeMap<Date, NavigableSet<InterestRateEntry>>
-	    (
-	     interestRateEntryStream()
-	     .filter(rateEntry ->
-		     (null != rateEntry.getDate())
-		     )
-	     .collect(Collectors
-		      .groupingByConcurrent(rateEntry ->
-					    rateEntry.getDate(),
-					    toSetByDuration()
-					    )
-		      )
-	     );
+        return
+            new TreeMap<Date, NavigableSet<InterestRateEntry>>
+            (
+             interestRateEntryStream()
+             .filter(rateEntry ->
+                     (null != rateEntry.getDate())
+                     )
+             .collect(Collectors
+                      .groupingByConcurrent(rateEntry ->
+                                            rateEntry.getDate(),
+                                            toSetByDuration()
+                                            )
+                      )
+             );
     }
     
     public Collection<InterestRateEntry> interestRateEntries()
     {
-	return
-	    interestRateEntryStream()
-	    .collect(Collectors.toList());
+        return
+            interestRateEntryStream()
+            .collect(Collectors.toList());
     }
 
     public static void main(String[] args)
-	throws IOException
+        throws IOException
     {
-	new BOFEuriborFetcher()
-	    .interestRateEntries()
-	    .forEach(e -> System.out.println(e));
+        new BOFEuriborFetcher()
+            .interestRateEntries()
+            .forEach(e -> System.out.println(e));
     }
 }
--- src/interest/image/InterestChart.java
+++ src/interest/image/InterestChart.java
@@ -16,177 +16,142 @@ public class InterestChart
 {
     public static final String myFormat = "yyyy-MM-dd";
     private static
-	String seriesName(Collection<? extends InterestRateEntry> series)
+        String seriesName(Collection<? extends InterestRateEntry> series)
     {
-	SimpleDateFormat myFormatter
-	    = new SimpleDateFormat(myFormat);
-	TreeSet<String> labels
-	    = new TreeSet<String>
-	    (series.parallelStream()
-	     .map(interestEntry ->
-		  (interestEntry.getLabel()
-		   + " "
-		   +myFormatter.format(interestEntry.getDate()
-				       )
-		   )
-		  )
-	     .collect(Collectors.toList()
-		      )
-	     );
-	StringBuilder sb = new StringBuilder();
-	boolean first = true;
-	for(String s : labels) {
-	    if(first)
-		first = false;
-	    else
-		sb.append(" ");
-	    sb.append(s);
-	}
-	return sb.toString();
+        SimpleDateFormat myFormatter
+            = new SimpleDateFormat(myFormat);
+        TreeSet<String> labels
+            = new TreeSet<String>
+            (series.stream()
+             .map(interestEntry ->
+                  (interestEntry.getLabel()
+                   + " "
+                   +myFormatter
+                   .format(interestEntry
+                           .getDate())
+                   )
+                  )
+             .collect(Collectors.toList()
+                      )
+             );
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for(String s : labels) {
+            if(first)
+                first = false;
+            else
+                sb.append(" ");
+            sb.append(s);
+        }
+        return sb.toString();
     }
 
     private static XYSeries
-    	addSeries(XYChart target,
-		  Collection<? extends InterestRateEntry> series)
-    //private static CategorySeries
-    //	addSeries(CategoryChart target, Collection<InterestRateEntry> series)
+        addSeries(XYChart target,
+                  Collection<? extends InterestRateEntry> series)
     {
-	String label
-	    = seriesName(series);
-	TreeSet<InterestRateEntry> myEntries
-	    = new TreeSet<InterestRateEntry>
-	    (Comparator.comparing(InterestRateEntry::getDurationDays));
-	myEntries.addAll(series);
-	ArrayList<Double> xDayValues
-	    = new ArrayList<Double>(myEntries.size());
-	ArrayList<Double> yRateValues
-	    = new ArrayList<Double>(myEntries.size());
-	InterestRateEntry origo
-	    = new InterestRateEntry(label,
-				    null,
-				    0.0d,
-				    0
-				    );
-	InterestRateEntry current = origo;
-	xDayValues.add((double) origo.getDurationDays());
-	yRateValues.add(origo.getRate());
-	for(InterestRateEntry latter : myEntries) {
-	    double currentLogRate
-		= Math.log1p(current.getRate());
-	    double latterLogRate
-		= Math.log1p(latter.getRate());
-	    double differenceLogRate
-		= latterLogRate - currentLogRate;
-	    int durationDayDifference
-		= latter.getDurationDays()
-		- current.getDurationDays();
-	    double averageLogRate
-		= differenceLogRate / durationDayDifference;
-	    double averageRate
-		= Math.expm1(averageLogRate);
-	    
-	    xDayValues.add((double) current.getDurationDays());
-	    yRateValues.add(averageRate);
-	    
-	    xDayValues.add((double) latter.getDurationDays());
-	    yRateValues.add(averageRate);
+        String label
+            = seriesName(series);
+        TreeSet<InterestRateEntry> myEntries
+            = new TreeSet<InterestRateEntry>
+            (Comparator.comparing(InterestRateEntry::getDurationDays));
+        myEntries.addAll(series);
 
-	    current = latter;
-	}
-	
-	XYSeries result
-	    = target.addSeries(label, xDayValues, yRateValues);
-	result
-	    .setXYSeriesRenderStyle(XYSeries
-				    .XYSeriesRenderStyle
-				    .Line
-				    );
-	
-	/*
-	CategorySeries result
-	    = target.addSeries(label, xDayValues, yRateValues);
-	result
-	    .setChartCategorySeriesRenderStyle(CategorySeries
-					  .CategorySeriesRenderStyle
-					  .SteppedBar
-					  );
-	*/
-	return result;
+        ArrayList<Double> xDayValues
+            = new ArrayList<Double>(myEntries.size());
+        ArrayList<Double> yRateValues
+            = new ArrayList<Double>(myEntries.size());
+
+        InterestRateEntry origo
+            = new InterestRateEntry
+            (label, null, 0.0d, 0);
+        xDayValues.add((double) origo.getDurationDays());
+        yRateValues.add(origo.getRate());
+
+        InterestRateEntry current = origo;
+        for(InterestRateEntry latter : myEntries) {
+            double currentLogRate
+                = Math.log1p(current.getRate());
+            double latterLogRate
+                = Math.log1p(latter.getRate());
+            double differenceLogRate
+                = latterLogRate - currentLogRate;
+            int durationDayDifference
+                = latter.getDurationDays()
+                - current.getDurationDays();
+            double averageLogRate
+                = differenceLogRate / durationDayDifference;
+            double averageRate
+                = Math.expm1(averageLogRate);
+            
+            xDayValues.add((double) current.getDurationDays());
+            yRateValues.add(averageRate);
+            
+            xDayValues.add((double) latter.getDurationDays());
+            yRateValues.add(averageRate);
+
+            current = latter;
+        }
+        
+        XYSeries result
+            = target.addSeries(label, xDayValues, yRateValues);
+        result
+            .setXYSeriesRenderStyle
+            (XYSeries
+             .XYSeriesRenderStyle
+             .Line
+             );
+
+        return result;
     }
 
     private List<? extends Collection<? extends InterestRateEntry>>
-	series;
+        series;
     private String label;
 
+    @SafeVarargs
     public InterestChart(Collection<? extends InterestRateEntry>... series)
     {
-	this(Arrays.asList(series));
+        this(Arrays.asList(series));
     }
     public
-	InterestChart(Collection<
-		      ? extends Collection<
-		      ? extends InterestRateEntry>> series)
+        InterestChart(Collection<
+                      ? extends Collection<
+                      ? extends InterestRateEntry>> series)
     {
-	this.series = new ArrayList(series);
-	label = "Interest Rates";
+        this.series
+            = new ArrayList<Collection<
+                ? extends InterestRateEntry>>(series);
+        label = "Interest Rates";
     }
-    /*    
-    public static XYChart
-	interestAreaChart(String label,
-			  Collection<InterestRateEntry>... series)
-    {
-	return interestAreaChart(label, Arrays.asList(series));
-    }
-    public static XYChart
-	interestAreaChart(String label,
-			  Collection<? extends Collection<InterestRateEntry>> series)
-    */
     public XYChart
-	interestAreaChart()
-    /*
-    public static CategoryChart
-	interestAreaChart(String label,
-			  Collection<InterestRateEntry>... series)
-    */
+        interestAreaChart()
     {
-	XYChart target
-	    = new XYChartBuilder()
-	    .title(label)
-	    //= new CategoryChartBuilder()
-	    .xAxisTitle("duration days")
-	    .yAxisTitle("rate")
-	    //	    .theme(Styler.ChartTheme.GGPlot2)
-	    .build();
-	for(Collection<? extends InterestRateEntry>
-		entryCollectionEntry : series) {
-	    addSeries(target,entryCollectionEntry);
-	}
-	return target;
+        XYChart target
+            = new XYChartBuilder()
+            .title(label)
+            .xAxisTitle("duration days")
+            .yAxisTitle("percentage rate")
+            .build();
+        for(Collection<? extends InterestRateEntry>
+                entryCollectionEntry : series) {
+            addSeries(target,entryCollectionEntry);
+        }
+        return target;
     }
 
     public static void main(String args[])
-	throws IOException
+        throws IOException
     {
-	InterestChart ic
-	    = new InterestChart(
-				new BOFEuriborFetcher()
-				.interestRateEntryMap()
-				.values()
-				);
-	XYChart chart
-	    //CategoryChart chart
-	    = ic.interestAreaChart();
-	//"DemoChart",
-	//.lastEntry().getValue()
-	//);
-	//JFrame frame
-	//=
-	new SwingWrapper(chart).displayChart();
-	    /*
-	frame.pack();
-	frame.setVisible(true);
-	    */
-	return;
+        InterestChart ic
+            = new InterestChart
+            (new BOFEuriborFetcher()
+             .interestRateEntryMap()
+             .values()
+             );
+        XYChart chart
+            = ic.interestAreaChart();
+        new SwingWrapper<XYChart>(chart).displayChart();
+        return;
     }
-    
 }
